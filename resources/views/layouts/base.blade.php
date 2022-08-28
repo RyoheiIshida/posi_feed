da0e14701bb9775fe610cc46d91627af8b0cce6b
ryoh <mw.iicd@gmail.com>
2022-08-28 19:22:57 +0900

    4th commit
    layouts

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCYTPE html>
+    <html lang='ja'>
+
+    <head>
+        <meta charset='UTF-8'>
+        <title>@yield('title')</title>
+
+    </head>
+    <body>
+        @section('main')
+        <p>デフォルトの文章</p>
+        @show
+</body>
