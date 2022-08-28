6834644abc241884cf7aa98194912adb4a0f5aaf
ryoh <mw.iicd@gmail.com>
2022-08-28 19:23:37 +0900

    5th commit
    sql

---
+++ sql/create.sql
@@ -0,0 +1,16 @@
+drop table users;
+CREATE TABLE users (
+    id INT(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(32) NOT NULL,
+    password VARCHAR(32) NOT NULL,
+    authority VARCHAR(32) NOT NULL
+)ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+
+create table feeds (
+    id int(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_users_id INT(32) NOT NULL,
+    posted_users_id INT(32) NOT NULL,
+    content text(140) NOT NULL,
+    private boolean not null DEFAULT true
+);
---
+++ sql/insert.sql
@@ -0,0 +1,3 @@
+insert into users (email,password,authority) values('aaa','passa','一般');
+insert into users (email,password,authority) values('bbb','passb','役員');
+insert into users (email,password,authority) values('ccc','passc','一般');
