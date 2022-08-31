4c6175b83611779108d38748182862de76b68881
ryoh <mw.iicd@gmail.com>
2022-08-31 18:19:28 +0900

    10th commit
    users index and create

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        $data = ['msg' => 'こんにちは、世界！！'];
+        $data = ['users' => User::find(10)];
         return view('users.index', $data);
+    }
+    public function create()
+    {
+        return view('users.create');    
+    }
+    public function store()
+    {
+        return redirect('users'); 
+    }
+    public function edit()
+    {
+        return view('users/edit');
+    }
+    public function view()
+    {
+        return view('users/vieww');
     }
 }
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+@section('title','ユーザー登録')
+@section('main')
+
+<h3>ユーザー登録</h3>
+<form method='POST' action='/users/store'>
+    @csrf
+    <label id='email'>Eメールアドレス</label>
+    <input id='email' name='email' type='text' size='32' value='{{old("email")}}' />
+    <br>
+    <label id='password'>パスワード</label>
+    <input id='password' name='password' type='text' size='32' value='{{old("password")}}' />
+    <br>
+    <label id='authority'>権限</label>
+    <input id='authority' name='authority' type='text' size='32' value='{{old("authority")}}' />
+    <br>
+    <input type='submit' value='送信' />
+</form>
+@endsection
