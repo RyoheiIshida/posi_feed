571ea6f4a59b1f0da8b3500a3183b81b2d8c85dc
ryoh <mw.iicd@gmail.com>
2022-09-25 15:52:07 +0900

    17th commit
    added $fillable to Feed

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
     use HasFactory;
+    protected $fillable = ['posted_users_id','created_users_id','content','private']; //保存したいカラム名が1つの場合
 
     public function users()
     {
