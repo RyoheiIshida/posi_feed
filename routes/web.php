ddbd97a8f7507dbaa693c807eac4ce120645b314
ryoh <mw.iicd@gmail.com>
2022-11-14 18:30:20 +0900

    26th commit
    routes/web

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/users','\App\Http\Controllers\UsersController@index');
+Route::get('/users/create','\App\Http\Controllers\UsersController@create');
+Route::post('/users/store','\App\Http\Controllers\UsersController@store');
+
+Route::get('/feeds','\App\Http\Controllers\FeedsController@index');
+Route::get('/feeds/create','\App\Http\Controllers\FeedsController@create');
+Route::post('/feeds/store','\App\Http\Controllers\FeedsController@store');
